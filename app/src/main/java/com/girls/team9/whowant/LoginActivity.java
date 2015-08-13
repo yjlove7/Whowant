18567a4c25a3e31976991a3e95a4468cb2604c58
eugene <7yjlove7@naver.com>
2015-08-14 07:01:02 +0900

    로그인페이지 회원가입페이지 연결

--- app/src/main/java/com/girls/team9/whowant/LoginActivity.java
+++ app/src/main/java/com/girls/team9/whowant/LoginActivity.java
@@ -1,22 +1,35 @@
 package com.girls.team9.whowant;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by eugene on 2015-08-14.
  */
 public class LoginActivity extends AppCompatActivity {
+    Button signup_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
+        signup_btn = (Button) findViewById(R.id.signup_btn);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("로그인");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+            }
+        });
 
 
     }
