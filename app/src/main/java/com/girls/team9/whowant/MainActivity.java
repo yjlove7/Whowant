83af9eac87f3a38a1ce6ae0ca17a96be07d59ac3
eugene <7yjlove7@naver.com>
2015-08-13 19:09:00 +0900

    splash 추가

--- app/src/main/java/com/girls/team9/whowant/MainActivity.java
+++ app/src/main/java/com/girls/team9/whowant/MainActivity.java
@@ -1,5 +1,6 @@
 package com.girls.team9.whowant;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, SplashActivity.class));
     }
 
     @Override
