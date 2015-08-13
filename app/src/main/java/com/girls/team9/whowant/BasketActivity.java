4a8daf4072092645bef543cf0fedc51569116c16
Han-Hm <hyemihan89@gmail.com>
2015-08-14 02:21:52 +0900

    activity intent 연결

--- app/src/main/java/com/girls/team9/whowant/BasketActivity.java
+++ app/src/main/java/com/girls/team9/whowant/BasketActivity.java
@@ -7,4 +7,5 @@ import android.support.v7.app.AppCompatActivity;
  * Created by hyemi on 2015-08-13.
  */
 public class BasketActivity extends AppCompatActivity {
+
 }//class
--- app/src/main/java/com/girls/team9/whowant/DetailAtivity.java
+++ app/src/main/java/com/girls/team9/whowant/DetailAtivity.java
@@ -1,9 +1,23 @@
 package com.girls.team9.whowant;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by hyemi on 2015-08-14.
  */
 public class DetailAtivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("상세 페이지");
+    }
+
 }//class
